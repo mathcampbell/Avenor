53ae50ca97341f7bc4538c3c09ffa5d49bb09d03
Math Campbell <math.campbell@gmail.com>
2026-07-27 19:56:13 +0100

    Add spline terrain foundation: update Avenor.Build.cs

--- AvenorClient/Source/Avenor/Avenor.Build.cs
+++ AvenorClient/Source/Avenor/Avenor.Build.cs
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 using UnrealBuildTool;
 
 public class Avenor : ModuleRules
@@ -7,17 +5,18 @@ public class Avenor : ModuleRules
 	public Avenor(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"ProceduralMeshComponent"
+			}
+		);
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 	}
 }
