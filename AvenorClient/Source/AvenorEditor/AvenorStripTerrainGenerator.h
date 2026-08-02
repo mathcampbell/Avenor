04fa509cc7e2b10aac7590d62cddc6f7a9d18f4b
Math Campbell <math.campbell@gmail.com>
2026-08-02 19:54:17 +0100

    Fix Mesh Partition modifier API include

--- AvenorClient/Source/AvenorEditor/AvenorStripTerrainGenerator.h
+++ AvenorClient/Source/AvenorEditor/AvenorStripTerrainGenerator.h
@@ -2,7 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "MeshPartition.h"
+#include "MeshPartitionModifierComponent.h"
 
 #include "AvenorStripTerrainGenerator.generated.h"
 
