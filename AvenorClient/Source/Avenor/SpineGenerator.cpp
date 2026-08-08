9e7d8c54477b7fc5d5d8ff903569c358b5baac2a
Math Campbell <math.campbell@gmail.com>
2026-08-08 21:25:40 +0100

    Fix Spine modifier bounds shadowing

--- AvenorClient/Source/Avenor/SpineGenerator.cpp
+++ AvenorClient/Source/Avenor/SpineGenerator.cpp
@@ -153,8 +153,10 @@ TArray<FBox> UAvenorSpineTerrainModifier::ComputeBounds() const
     {
         return {};
     }
-    const FBox Bounds = Spine->GetTerrainCorridorBounds();
-    return Bounds.IsValid ? TArray<FBox>{Bounds} : TArray<FBox>{};
+    const FBox CorridorBounds = Spine->GetTerrainCorridorBounds();
+    return CorridorBounds.IsValid
+        ? TArray<FBox>{CorridorBounds}
+        : TArray<FBox>{};
 }
 
 TSharedPtr<const UE::MeshPartition::IModifierBackgroundOp>
